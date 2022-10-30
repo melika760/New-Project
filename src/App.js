3f6bd90f0e253ff1077cd3e50b1c1d74d63eb011
melika760 <72795928+melika760@users.noreply.github.com>
2022-10-30 13:09:13 +0330

    add css

--- src/App.js
+++ src/App.js
@@ -24,9 +24,11 @@ function App() {
   ];
   return (
     <div className="App">
+      <div className='expenses'>
   <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
   <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
   <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+  </div>
     </div>
   );
 }
