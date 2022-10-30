264fb13ed9234bc1a7ac1acdb4bbe642097f275d
melika760 <72795928+melika760@users.noreply.github.com>
2022-10-30 19:04:09 +0330

    add component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import ExpenseItem from './Components/ExpenseItem';
+import Expenses from './Expenses';
 function App() {
   const expenses = [
     {
@@ -24,11 +24,7 @@ function App() {
   ];
   return (
     <div className="App">
-      <div className='expenses'>
-  <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-  <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-  <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-  </div>
+   <Expenses items={expenses}/>
     </div>
   );
 }
