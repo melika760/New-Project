a52bca3edf2fab5952315bae90e624b9839d8ef7
melika760 <72795928+melika760@users.noreply.github.com>
2022-11-08 18:49:28 +0330

    child to parent

--- src/Components/NewExpenses/ExpensesForm.js
+++ src/Components/NewExpenses/ExpensesForm.js
@@ -1,19 +1,43 @@
+import React,{useState} from 'react'
 import'./ExpenseForm.css'
-export default function ExpenseForm(){
-    
+export default function ExpenseForm(props){
+const[enteredtitle,SetTitle]=useState('');
+const[enteredamount,SetAmount]=useState('')
+const[enteredDate,SetDate]=useState('')
+    function titleChangeHandler(event){
+SetTitle(event.target.value)
+    }
+    function amountChangeHandler(event){
+        SetAmount(event.target.value)
+    }
+function dateChangeHandler(event){
+    SetDate(event.target.value)
+}
+function SubmitHandler(event){
+    event.preventDefault();
+    const ExpenseData ={
+        title:enteredtitle,
+        Amount:enteredamount,
+        Date:new Date(enteredDate)
+    }
+    props.onSaveExpenseData(ExpenseData);
+    SetAmount("");
+    SetTitle("");
+    SetDate('')
+}
     return(
-        <form>
-    <div className='new-expense__controls'>
+        <form onSubmit={SubmitHandler}>
+    <div className='new-expense__controls'onChange={titleChangeHandler}>
 <label>Title</label>
 <input type="text"/>
     </div>
     <div className='new-expense__controls'>
 <label>Amount</label>
-<input type="number" min="0.01" step='0.01'/>
+<input type="number" min="0.01" step='0.01'onChange={amountChangeHandler}/>
     </div>
     <div className='new-expense__controls'>
 <label>Date</label>
-<input type='date' min='2019-01-01' max='2022-12-31'/>
+<input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}/>
     </div>
     <div className='new-expense__actions'>
         <button type='submit'>Add Expense</button>
--- src/Components/NewExpenses/NewExpenses.js
+++ src/Components/NewExpenses/NewExpenses.js
@@ -1,7 +1,15 @@
 import './NewExpenses.css'
 import ExpenseForm from './ExpensesForm'
 export default function NewExpenses(){
+    function expenseDataHandler(enteredExpenseData){
+        const ExpenseData ={
+            ...enteredExpenseData,
+            id:Math.random().toString()
+        }
+        console.log(ExpenseData)
+
+    }
     return(<div className='new-expense'>
-        <ExpenseForm/>
+        <ExpenseForm onSaveExpenseData={expenseDataHandler}/>
     </div>)
 }
